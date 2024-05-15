43151a69d64919c21ae0a1a710e2412c3ce5fd06
Mushfiq Alam Sami <mushfiq9912@gmail.com>
2024-05-15 23:58:40 +0600

    modified all lorem ipsum text to meaningful text

--- src/components/Comments/Comment.jsx
+++ src/components/Comments/Comment.jsx
@@ -6,12 +6,14 @@ const Comment = ({ comment }) => {
     const { user } = useContext(AuthContext);
     const { _id, userName, body, userEmail, userPhoto } = comment;
     return (
-        <div className="my-4 ml-4">
-            <div>
+        <div className="my-4 ml-4 flex flex-row items-center">
+            <div className="flex flex-row items-center gap-4">
                 <img src={userPhoto} className=" w-16 h-16 rounded-full" alt="" />
-                <h4 className="" title={userEmail}>{userName}Commented:</h4>
+                <h4 className="" title={userEmail}><span className="font-bold text-base">{userName}</span> Commented:</h4>
             </div>
-            <p className="">{body}</p>
+            <div>
+                <p className="ml-2">{body}</p>
+            </div>
 
         </div>
     );
